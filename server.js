80661978a60b1087c5ccaaac7103127e17d24d79
gisosa <gio53196@gmail.com>
2021-08-22 22:29:35 -0700

    Added middleware and comments in server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 // dependencies acquired to access routes
 const express = require('express');
-const api = require('./routes/api');
-const html = require('./routes/html');
+
+//access js files
+require('./routes/api');
+require('./routes/html');
 
 //initializes express app and creates port for heroku
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Setup listener
+app.use('/api', api);
+app.use('/', html);
+
+// access public folder 
+app.use(express.static('public'));
+
+// Start setup listener on port
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
